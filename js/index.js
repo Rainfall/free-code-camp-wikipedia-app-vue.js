901aeb00306300e5b3775cce6bc998ef4f229e9f
Denis Raygoroskiy <paradoxetion@gmail.com>
2019-01-03 16:39:12 +0300

    search feacher and styling

--- js/index.js
+++ js/index.js
@@ -4,8 +4,23 @@ new Vue({
   el: '#app',
   data: {
     relatedImgUrl: 'https://images.unsplash.com/photo-1533612773272-7230cca4169e',
+    searchWord: '',
+    searchResults: [],
   },
   methods: {
+    searchWiki() {
+      if (this.searchWord != '') {
+        this.$http.get('//en.wikipedia.org/w/api.php?action=opensearch&origin=*&search='+this.searchWord).then(response => {
+          console.log(response.body);
+          this.searchResults = response.body;
+          this.relatedImgUrl = 'https://source.unsplash.com/featured/?' + this.searchWord
+        })
+      } else {
+        this.searchResults = [];
+        this.relatedImgUrl = 'https://images.unsplash.com/photo-1533612773272-7230cca4169e';
+        alert('Please input search word');
+      }
+    }
   },
 
 })
